0841177ecc0732869305a68ad5537d57c5e7c897
bakary <bakarydiarra8509@gmail.com>
2020-09-10 21:52:24 +0200

    refacto ..

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,15 +6,22 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => ' Images PNG OR JPG file',
+                'required' => false,
+                'allow_delete' => false ,
+                'imagine_pattern' => 'my_thumb_filter_min',
+                'download_label' => 'Télécharger ',
+            ])
             ->add('name')
             ->add('description')
-            ->add('image')
             ->add('etat')
             ->add('price')
         ;
