6bbcbd0e3f68ab1fef2667a163701ab958111867
bakary <bakarydiarra8509@gmail.com>
2020-09-15 03:11:27 +0200

    add concurrence

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -14,18 +14,6 @@ class AccueilController extends AbstractController
      */
     public function index(ProduitRepository $produitRepository) :Response
     {
-        $produits = $produitRepository->findAll();
-
-        foreach ($produits as $produit){
-
-            if ($produit->getConcurrence() !== null)
-            if ($produit->getEtat()->getName() === $produit->getConcurrence()->getEtat()){
-                if ($produit->getPrice()->getPrixNormal() > $produit->getConcurrence()->getPrix()){
-                    $prixNormal = $produit->getConcurrence()->getPrix() - 1;
-                }
-            }
-
-        }
         return $this->render('accueil/index.html.twig', [
             'produits' => $produitRepository->findAll(),
         ]);
